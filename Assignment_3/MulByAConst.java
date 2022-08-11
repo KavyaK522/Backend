7760d263a716bd0d41846c440de4d089a367c046
Kavya K <kk@orthofx.com>
2022-08-11 10:39:44 +0530

    Matrix mul by a const.

---
+++ Assignment_3/MulByAConst.java
@@ -0,0 +1,34 @@
+package com.orthofx;
+
+import java.util.Scanner;
+
+public class MulByAConst {
+	public static void main(String[] args) {
+		Scanner a=new Scanner(System.in);
+		System.out.println("Enter the no. of rows of matrix-1 :-");
+		int m=a.nextInt();
+		System.out.println("Enter the no. of columns of matrix-1 :-");
+		int n=a.nextInt();
+		System.out.println("Enter the constant :-");
+		int c=a.nextInt();
+		int x[][]= new int[m][n];
+		int y[][]= new int[m][n];
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				x[i][j]=a.nextInt();
+			}
+		}
+		for(int i=0;i<m;i++) {
+			 for(int j=0;j<n;j++) {
+				 y[i][j]=c*x[i][j];
+			 }
+		}
+		System.out.println("Result:-");
+		 for(int i=0;i<m;i++) {
+			 for(int j=0;j<n;j++) {
+				 System.out.print(y[i][j]+" ");
+			 }
+			 System.out.println();
+	     }
+	}
+}
