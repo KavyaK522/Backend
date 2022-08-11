ed15debcb79d27633098d7311080bb2e23560a4a
Kavya K <kk@orthofx.com>
2022-08-11 11:44:54 +0530

    Assignment-3

--- Assignment_3/MatrixMul.java
+++ Assignment_3/MatrixMul.java
@@ -2,7 +2,7 @@ package com.orthofx;
 
 import java.util.Scanner;
 
-public class MatrixAddition {
+public class MatrixMul {
 	public static void main(String[] args) {
 		Scanner a=new Scanner(System.in);
 		System.out.println("Enter the no. of rows of matrix-1 :-");
@@ -13,39 +13,38 @@ public class MatrixAddition {
 		int o=a.nextInt();
 		System.out.println("Enter the no. of columns of matrix-2 :-");
 		int p=a.nextInt();
-		if(m==o && n==p) {
+		if(n==o) {
 			int x[][]= new int[m][n];
 			int y[][]= new int[o][p];
-			int z[][]= new int[m][n];
+			int z[][]= new int[m][p];
 		System.out.println("Enter the elements of matrix-1:-");	
 		  for(int i=0;i<m;i++) {
 			for(int j=0;j<n;j++) {
 				x[i][j]=a.nextInt();
 			}
 		  }
-		System.out.println("Enter the elements of matrix-2:-");
+		System.out.println("Enter the elements of matrix-2:-");  
 		  for(int i=0;i<o;i++) {
 			for(int j=0;j<p;j++) {
 				y[i][j]=a.nextInt();
 			} 
 		  }
-		 for(int i=0;i<m;i++) {
-			 for(int j=0;j<n;j++) {
-				 z[i][j]=x[i][j]+y[i][j];
-			 }
-		 }
-		 System.out.println("Added result:-");
-		 for(int i=0;i<m;i++) {
-			 for(int j=0;j<n;j++) {
-				 System.out.print(z[i][j]+" ");
-			 }
-			 System.out.println();
-		 }
+		  for(int i=0;i<m;i++) {
+			for(int j=0;j<p;j++) {
+				for(int k=0;k<n;k++) {
+					z[i][j]+=x[i][k]*y[k][j];
+			    }
+		    }
+		  }
+		  for(int i=0;i<m;i++) {
+			  for(int j=0;j<p;j++) {
+				  System.out.print(z[i][j]+" ");
+			  }
+			  System.out.println();
+		  }
 		}
 		else {
 			System.out.println("Invalid input");
 		}
 	}
 }
-		
-		
--- Assignment_3/MulByAConst.java
+++ Assignment_3/MulByAConst.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class MulByAConst {
 	public static void main(String[] args) {
 		Scanner a=new Scanner(System.in);
-		System.out.println("Enter the no. of rows of matrix-1 :-");
+		System.out.println("Enter the no. of rows of matrix :-");
 		int m=a.nextInt();
-		System.out.println("Enter the no. of columns of matrix-1 :-");
+		System.out.println("Enter the no. of columns of matrix  :-");
 		int n=a.nextInt();
 		System.out.println("Enter the constant :-");
 		int c=a.nextInt();
 		int x[][]= new int[m][n];
 		int y[][]= new int[m][n];
+		System.out.println("Enter the elements of the matrix:-");
 		for(int i=0;i<m;i++) {
 			for(int j=0;j<n;j++) {
 				x[i][j]=a.nextInt();
